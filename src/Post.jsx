76bac75cd7528cfd8fc78b6a19536c6d692736cd
Sashankr <sashankr97@gmail.com>
2022-01-01 18:05:45 +0530

    added individual post component

--- src/Post.jsx
+++ src/Post.jsx
@@ -1,27 +1,27 @@
-// import React from 'react';
-// import {useQuery} from 'react-query';
+import React from 'react';
+import {useQuery} from 'react-query';
 
-// const fetcher = (url) =>{
-//     return fetch(url).then(data => data.json())
-// }
+const fetcher = (url) =>{
+    return fetch(url).then(data => data.json())
+}
 
 
-// const Post = ({postID,goBack}) => {
+const Post = ({postID,goBack}) => {
 
-//     const {data,isLoading} = useQuery(['individualPost',postID], () => fetcher(`https://jsonplaceholder.typicode.com/posts/${postID}`))
+    const {data,isLoading} = useQuery(['individualPost',postID], () => fetcher(`https://jsonplaceholder.typicode.com/posts/${postID}`))
 
-//     if(isLoading ){
-//         return <h2>Loading...</h2>
-//     }
+    if(isLoading ){
+        return <h2>Loading...</h2>
+    }
 
-//     return (
-//         <div class="individual-post">
-//             <a href="#" onClick={goBack}>Go Back</a>
-//             <p><small>{data.id}</small></p>
-//             <h2>{data.title} </h2>
-//             <p>{data.body}</p>
-//         </div>
-//     )
-// }
+    return (
+        <div class="individual-post">
+            <a href="#" onClick={goBack}>Go Back</a>
+            <p><small>{data.id}</small></p>
+            <h2>{data.title} </h2>
+            <p>{data.body}</p>
+        </div>
+    )
+}
 
-// export default Post;
+export default Post;
